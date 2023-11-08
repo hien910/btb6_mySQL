369846609ec160d0f5d7705c81ec8e02ec9c8719
hien910 <hienjang910@gmail.com>
2023-11-08 19:55:10 +0700

    Update bt4b6.sql

--- bt4b6.sql
+++ bt4b6.sql
@@ -6,48 +6,52 @@ where first_name like 'S%';
 select * from film 
 where release_year between 2000 and 2010;
 
--- Lấy danh sách các bộ phim (films) có rating là “PG” hoặc “PG-13”.
+-- 3. Lấy danh sách các bộ phim (films) có rating là “PG” hoặc “PG-13”.
 select * from film
 where rating = 'PG' or rating = 'PG-13';
 
--- Lấy danh sách diễn viên (actors) và số lượng bộ phim (films) mà họ tham gia, sắp xếp theo số lượng bộ phim giảm dần.
+-- 4. Lấy danh sách diễn viên (actors) và số lượng bộ phim (films) mà họ tham gia, sắp xếp theo số lượng bộ phim giảm dần.
 select actor.* , count(*) as num_of_movies from actor
 inner join film_actor on actor.actor_id = film_actor.actor_id
 inner join film on film_actor.film_id = film.film_id
 group by actor_id;
 
 
--- Lấy danh sách các bộ phim (films) và thể loại (categories) của chúng.
+-- 5. Lấy danh sách các bộ phim (films) và thể loại (categories) của chúng.
 select film.*, category.name from film
 inner join film_category on film.film_id = film_category.film_id
 inner join category on film_category.category_id = category.category_id;
  
 
 
--- Lấy danh sách các bộ phim (films) và tổng số diễn viên (actors) tham gia trong mỗi bộ phim, sắp xếp theo tổng số diễn viên giảm dần.
+-- 6. Lấy danh sách các bộ phim (films) và tổng số diễn viên (actors) tham gia trong mỗi bộ phim, sắp xếp theo tổng số diễn viên giảm dần.
 select film.* , count(*) as num_of_participants from actor
 inner join film_actor on actor.actor_id = film_actor.actor_id
 inner join film on film_actor.film_id = film.film_id
 group by film_id
 order by num_of_participants DESC;
 
--- Lấy danh sách các diễn viên (actors) có số lượng bộ phim (films) tham gia lớn hơn 30.
+-- 7. Lấy danh sách các diễn viên (actors) có số lượng bộ phim (films) tham gia lớn hơn 30.
 select actor.* , count(*) as num_of_movies from actor
 inner join film_actor on actor.actor_id = film_actor.actor_id
 inner join film on film_actor.film_id = film.film_id
 group by actor_id
 having num_of_movies > 30;
 
--- Lấy danh sách các diễn viên (actors) không tham gia trong bất kỳ bộ phim nào.
+-- 8. Lấy danh sách các diễn viên (actors) không tham gia trong bất kỳ bộ phim nào.
 select actor.* , count(*) as num_of_movies from actor
 inner join film_actor on actor.actor_id = film_actor.actor_id
 inner join film on film_actor.film_id = film.film_id
 group by actor_id
 having num_of_movies = 0;
 
--- Lấy danh sách bộ phim (films) và tổng doanh thu (revenue) của từng bộ phim, sắp xếp theo tổng doanh thu giảm dần.
+-- 9. Lấy danh sách bộ phim (films) và tổng doanh thu (revenue) của từng bộ phim, sắp xếp theo tổng doanh thu giảm dần.
+SELECT film_id, title, SUM(rental_duration * rental_rate) as revenue 
+FROM film
+GROUP BY film_id, title
+ORDER BY revenue DESC;
 
--- Lấy danh sách các bộ phim (films) và thể loại (categories) của chúng, với điều kiện mỗi bộ phim thuộc thể loại “Horror”.
+-- 10. Lấy danh sách các bộ phim (films) và thể loại (categories) của chúng, với điều kiện mỗi bộ phim thuộc thể loại “Horror”.
 select film.* , category.name from film
 inner join film_category on film.film_id = film_category.film_id
 inner join category on film_category.category_id = category.category_id
